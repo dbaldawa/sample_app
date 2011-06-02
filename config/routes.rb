22c09a69340a61dedb0ba42ceb5c1841bd978bc0
Deepak Baldawa <deepakbaldawa@yahoo.co.in>
2011-06-03 00:33:02 +0530

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
-  match '/signup', :to => 'users#new'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
   root :to => 'pages#home'
   
@@ -16,7 +18,7 @@ SampleApp::Application.routes.draw do
 
   get "pages/about"
   
-  get "users/new"
+#  get "users/show"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
