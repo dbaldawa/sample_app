5bba8f2c0ba53e1b1c968e4dcdaef652f2aaa634
Deepak Baldawa <deepakbaldawa@yahoo.co.in>
2011-06-12 02:29:08 +0530

    Sign-in-out changes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
-  root :to => 'pages#home'
+  root              :to => 'pages#home'
   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -17,8 +22,6 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
 
   get "pages/about"
-  
-#  get "users/show"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
